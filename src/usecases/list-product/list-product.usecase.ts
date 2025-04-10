c0fdc9bd184a4c3536815d69f995f052fac93ae3
Emilly Paiva <emillypaiva3260@gmail.com>
2025-04-10 14:46:48 -0300

    list usecase feito

---
+++ src/usecases/list-product/list-product.usecase.ts
@@ -0,0 +1,60 @@
+import { Product } from "../../domain/entity/product";
+import { ProductGateway } from "../../domain/gateway/procudt.gateway";
+import { Usecase } from "../usecase";
+
+export type ListProductInputDto = void;
+
+
+export type ListProductOutDto = {
+    products: {
+        id: string;
+        name: string;
+        price: number;
+        quantity: number;
+
+    }[];
+}
+
+
+export class ListProductUsecase implements Usecase<ListProductInputDto, ListProductOutDto>{
+    private constructor(private readonly productGateway: ProductGateway){}
+
+
+
+    public static create(productGateway: ProductGateway){
+
+        return new ListProductUsecase(productGateway);
+
+    }
+
+
+    public async execute(): Promise<ListProductOutDto>{
+
+        const aProducts = await this.productGateway.list();
+
+        const output = this.presentOutput(aProducts);
+
+        return output;
+
+    }
+
+
+    private presentOutput( products: Product[]): ListProductOutDto{
+        return {
+            products: products.map((p) => {
+                return {
+                    id: p.id,
+                    name: p.name,
+                    price: p.price,
+                    quantity: p.quantity
+                    
+                }
+            })
+        }
+
+    }
+}
+
+
+
+
