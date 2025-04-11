dcfbf63d4ee0948d9eef39b4fc1938ffacc80a1b
Emilly Paiva <emillypaiva3260@gmail.com>
2025-04-11 08:45:42 -0300

    :sparkles: list product route

--- src/usecases/list-product/list-product.usecase.ts
+++ src/usecases/list-product/list-product.usecase.ts
@@ -4,57 +4,42 @@ import { Usecase } from "../usecase";
 
 export type ListProductInputDto = void;
 
-
-export type ListProductOutDto = {
+export type ListProductOutputDto = {
     products: {
         id: string;
         name: string;
         price: number;
         quantity: number;
-
     }[];
-}
+};
 
+export class ListProductUsecase
+    implements Usecase<ListProductInputDto, ListProductOutputDto>
+{
+    private constructor(private readonly productGateway: ProductGateway) {}
 
-export class ListProductUsecase implements Usecase<ListProductInputDto, ListProductOutDto>{
-    private constructor(private readonly productGateway: ProductGateway){}
-
-
-
-    public static create(productGateway: ProductGateway){
-
+    public static create(productGateway: ProductGateway) {
         return new ListProductUsecase(productGateway);
-
     }
 
-
-    public async execute(): Promise<ListProductOutDto>{
-
+    public async execute(): Promise<ListProductOutputDto> {
         const aProducts = await this.productGateway.list();
 
         const output = this.presentOutput(aProducts);
 
         return output;
-
     }
 
-
-    private presentOutput( products: Product[]): ListProductOutDto{
+    private presentOutput(products: Product[]): ListProductOutputDto {
         return {
             products: products.map((p) => {
                 return {
                     id: p.id,
                     name: p.name,
                     price: p.price,
-                    quantity: p.quantity
-                    
-                }
-            })
-        }
-
+                    quantity: p.quantity,
+                };
+            }),
+        };
     }
 }
-
-
-
-
